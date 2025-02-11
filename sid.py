36ee4fe3fdca527a65abd1a90802feef87ef6b1d
adity72dd <mmunnabot7@gmail.com>
2025-02-11 14:24:21 +0530

    Update sid.py

--- sid.py
+++ sid.py
@@ -8,9 +8,9 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from config import BOT_TOKEN, ADMIN_IDS, OWNER_USERNAME
 
 USER_FILE = "users.json"
-DEFAULT_THREADS = 1300
-DEFAULT_PACKET = 10
-DEFAULT_DURATION = 180  # Set default duration (e.g., 60 seconds)
+DEFAULT_THREADS = 2000
+DEFAULT_PACKET = 16
+DEFAULT_DURATION = 120  # Set default duration (e.g., 60 seconds)
 
 users = {}
 user_processes = {}  # Dictionary to track processes for each user
